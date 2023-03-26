fcc3ad218f4b94d37991d38764e011990ea90cc0
Jeonggyu98 <kimjg_98@naver.com>
2023-03-26 22:27:02 +0900

    단어 길이 재기

---
+++ 문자열/단어 길이 재기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        System.out.println(str.length());
+
+    }
+}

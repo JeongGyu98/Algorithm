affe689ee34731432b45791d012a06f85e6894cd
Jeonggyu98 <kimjg_98@naver.com>
2023-03-27 15:20:38 +0900

    네 수

---
+++ 문자열/네 수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String[] input = br.readLine().split(" ");
+
+        long num1 = Long.parseLong(input[0] + input[1]);
+        long num2 = Long.parseLong(input[2] + input[3]);
+
+        sb.append(num1 + num2);
+
+        System.out.println(sb);
+    }
+}

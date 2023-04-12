a9904070c5075941e2702fbf0044cab41cce5a9f
Jeonggyu98 <kimjg_98@naver.com>
2023-04-12 22:05:19 +0900

    1,2,3 더하기

---
+++ 브루트포스/1, 2, 3 더하기.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.nio.Buffer;
+import java.util.*;
+public class Main {
+
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+      int T = Integer.parseInt(br.readLine());
+      StringBuilder sb = new StringBuilder();
+
+      for (int i = 0; i < T; i++) {
+         int n = Integer.parseInt(br.readLine());
+         int[] dp = new int[11];
+
+         dp[0] = 0;
+         dp[1] = 1;
+         dp[2] = 2;
+         dp[3] = 4;
+
+         for (int j = 4; j <= n; j++) {
+            dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
+         }
+
+         sb.append(dp[n]).append("\n");
+      }
+
+      System.out.println(sb);
+   }
+
+
+}

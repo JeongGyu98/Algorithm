609c6923ef2e51c45e069f04921bf7a15865f87e
Jeonggyu98 <kimjg_98@naver.com>
2023-05-04 14:17:58 +0900

    단어변환

---
+++ DFS, BFS/단어변환.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Solution {
+   static int answer = 0;
+   static boolean[] visited;
+   public int solution(String begin, String target, String[] words) {
+
+      visited = new boolean[words.length];
+
+      dfs(begin, target, words, 0);
+
+      return answer;
+   }
+
+   public void dfs(String begin, String target, String[] words, int count){
+
+      if(begin.equals(target)){
+         answer = count;
+         return;
+      }
+
+      for(int i = 0; i < words.length; i++){
+
+         if(visited[i]) continue;
+
+         int same = 0;
+         for(int j = 0; j < begin.length(); j++){
+            if(begin.charAt(j) == words[i].charAt(j)){
+               same++;
+            }
+         }
+         if(same == begin.length() - 1){
+            visited[i] = true;
+            dfs(words[i], target, words, count + 1);
+            visited[i] = false;
+         }
+      }
+   }
+}
